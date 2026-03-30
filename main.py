8b6ab6f28c146cd7e4b1591dce7a6285cf8e7c46
Luiz Otavio Lima <luizotavioflima@gmail.com>
2026-03-30 18:04:44 -0300

    Criando uma sequencia em cadeia com LCEL

--- main.py
+++ main.py
@@ -18,7 +18,15 @@ class Destiny (BaseModel):
         description="Motivo pelo qual é interessante visitar essa cidade")
 
 
-parser = JsonOutputParser(pydantic_object=Destiny)
+class Restaurants (BaseModel):
+    cidade: str = Field(description="A cidade recomendada para visitar")
+    restaurantes: str = Field(
+        description="Restaurantes recomendados na cidade")
+
+
+destiny_parser = JsonOutputParser(pydantic_object=Destiny)
+restaurants_parser = JsonOutputParser(pydantic_object=Restaurants)
+
 
 prompt_city = PromptTemplate(
     template="""
@@ -26,16 +34,29 @@ prompt_city = PromptTemplate(
     {output_format}
     """,
     input_variables=["interesse"],
-    partial_variables={"output_format": parser.get_format_instructions()}
+    partial_variables={"output_format": destiny_parser.get_format_instructions()})
 
-)
+prompt_restaurants = PromptTemplate(
+    template="""
+    Sugira restaurantes populares entre locais em {cidade}.
+    {output_format}
+    """,
+    partial_variables={"output_format": restaurants_parser.get_format_instructions()})
+
+prompt_culture = PromptTemplate(
+    template="""Sugira atividades e locais culturais para em {cidade}.""")
+
 
 model = ChatOpenAI(
     model="gpt-3.5-turbo",
     temperature=0.5,
     api_key=api_key)
 
-chain = prompt_city | model | parser
+chain_1 = prompt_city | model | destiny_parser
+chain_2 = prompt_restaurants | model | restaurants_parser
+chain_3 = prompt_culture | model | StrOutputParser()
+
+chain = (chain_1 | chain_2 | chain_3)
 
 response = chain.invoke(
     {"interesse": "praias"}
